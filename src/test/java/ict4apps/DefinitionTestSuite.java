3a282a9045628f06b2526c3741c55eb86d1a1bb6
Vladimir Kulikov <kulikovvladimir@hotmail.com>
2017-03-31 16:04:47 +0300

    (31/03) second

--- src/test/java/ict4apps/DefinitionTestSuite.java
+++ src/test/java/ict4apps/DefinitionTestSuite.java
@@ -7,15 +7,15 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {
         "src/test/resources/features/WELCOME_PAGE.feature",
-        "src/test/resources/features/PRODUCTS_PAGE.feature",
-        "src/test/resources/features/SPECIAL_OFFERS_PAGE.feature",
-        "src/test/resources/features/BLOGS_PAGE.feature",
-        "src/test/resources/features/CONTACT_US_PAGE.feature",
-        "src/test/resources/features/FOOTER.feature",
-        "src/test/resources/features/HEADER.feature",
-        "src/test/resources/features/SCROLLED_HEADER.feature",
-        "src/test/resources/features/BREADCRUMB.feature",
-        "src/test/resources/features/SEARCH_PAGE.feature"
+//        "src/test/resources/features/PRODUCTS_PAGE.feature",
+//        "src/test/resources/features/SPECIAL_OFFERS_PAGE.feature",
+//        "src/test/resources/features/BLOGS_PAGE.feature",
+//        "src/test/resources/features/CONTACT_US_PAGE.feature",
+//        "src/test/resources/features/FOOTER.feature",
+//        "src/test/resources/features/HEADER.feature",
+//        "src/test/resources/features/SCROLLED_HEADER.feature",
+//        "src/test/resources/features/BREADCRUMB.feature",
+//        "src/test/resources/features/SEARCH_PAGE.feature"
 })
 public class DefinitionTestSuite {
 }
