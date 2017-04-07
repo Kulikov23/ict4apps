396b5b607e817d0e4904dac96f252aee48c6c6c1
Vladimir Kulikov <kulikovvladimir@hotmail.com>
2017-04-07 17:23:34 +0300

    (07/04) fourth

--- src/test/java/ict4apps/DefinitionTestSuite.java
+++ src/test/java/ict4apps/DefinitionTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {
         "src/test/resources/features/WELCOME_PAGE.feature",
-//        "src/test/resources/features/PRODUCTS_PAGE.feature",
+        "src/test/resources/features/PRODUCTS_PAGE.feature",
         "src/test/resources/features/SPECIAL_OFFERS_PAGE.feature",
         "src/test/resources/features/BLOGS_PAGE.feature",
         "src/test/resources/features/CONTACT_US_PAGE.feature",
